7f6c43d47b081ea994d68ba31a68d18bee42750a
JimmyBoyle <jimmy@theboylefamily.net>
2018-09-11 13:34:03 -0700

    better fix

--- non-price-per-hour.js
+++ non-price-per-hour.js
@@ -16,44 +16,6 @@ $('.aws-plc-content').on("DOMSubtreeModified", function () {
 });
 
 function modifyTable() {
-    /*
-    if (!doneModify) {
-        if (first) {
-            let x = ($($($($(".aws-controls")[0]).find('.button'))[0].children))
-            $.each(x, function (index, value) {
-                console.log("val")
-                if ($(this).attr('class') == "js-active") {
-                    prevRegion = this.innerText
-                    regionsUsed.add(prevRegion);
-                    first = false;
-                    console.log("First")
-                    console.log(regionsUsed)
-                }
-            })
-
-        }
-    
-        
-        let x = $(".aws-controls");
-        $.each(x, function (index, value) {
-            $($($(value).find('.button'))[0].children).click(function () {
-                if (prevRegion != this.innerHTML) {
-                    prevRegion == this.innerHTML
-                    if ($(this).attr('class') != "js-active") {
-                        if (!regionsUsed.has(this.innerText) & runOnce) {
-                            runOnce = true;
-                            console.log('inside')
-                            regionsUsed.add(this.innerText)
-                            window.clearTimeout(timeoutHandle);
-                            timeoutHandle = window.setTimeout(modifyTable, 3 * 1000);
-                        }
-                        console.log(regionsUsed)
-                    }
-                }
-            })
-        });
-    }
-    */
     if (!doneModify) {
         let x = $(".aws-controls");
         $.each(x, function (index, value) {
@@ -69,23 +31,17 @@ function modifyTable() {
     }
     doneModify = true
 
-    var table_headers = $('tr:contains("Price Per Hour")')
-    let removeIndicies = []
+    var table_headers = $('tr:contains("Price Per Hour")').not('tr:contains("Price Per Month")')
     $.each(table_headers, function (index, value) {
-        if (value.innerText.indexOf('Price Per Month') >= 0) {
-            removeIndicies.push(index)
-        } else {
-            value.innerHTML = value.innerHTML + '<th>Price Per Month</th>';
-        }
+        value.innerHTML = value.innerHTML + '<th>Price Per Month</th>';
     });
 
     var cells = table_headers.parent().parent().find('tr[data-plc-offer-id]');
-    console.log(cells)
     var pattern = new RegExp("^\\$(\\d+\\.\\d+)$");
     $.each(cells, function (index, value) {
         var message = $(value).find('td:last-child')[0].innerText;
         let matches = pattern.exec(message);
-        if (matches != null && matches.length > 1 && value.innerHTML.split("<td>").length <= 3) {
+        if (matches != null && matches.length > 1) {
             let cost = parseFloat(matches[1]);
             let monthlyCost = (cost * 24 * 30.5).toFixed(2);
             value.innerHTML = value.innerHTML + '<td>$' + monthlyCost + '</td>';
