b306ad22dc4a474272c48c51fd8d7695637602ba
JimmyBoyle <jimmy@theboylefamily.net>
2018-09-12 14:53:34 -0700

    working for RDS/Aurora
    Need final cleanup before merge

--- non-price-per-hour.js
+++ non-price-per-hour.js
@@ -1,13 +1,9 @@
 
 var timeoutHandle = window.setTimeout(error, 20 * 1000);
 var doneModify = false;
-var regionsUsed = new Set();
-var prevRegion = '';
-var first = true;
-var runOnce = true
 
 $('.aws-plc-content').on("DOMSubtreeModified", function () {
-
+    console.log("jimmy is fuckckcckksdfl;jsdfj")
 
     if (!doneModify) {
         window.clearTimeout(timeoutHandle);
@@ -24,29 +20,35 @@ function modifyTable() {
                 if ($(this).attr('class') != "js-active") {
                     window.clearTimeout(timeoutHandle);
                     timeoutHandle = window.setTimeout(modifyTable, 3 * 1000);
-                }
+                };
 
-            })
+            });
         });
     }
     doneModify = true
 
     var table_headers = $('tr:contains("Price Per Hour")').not('tr:contains("Price Per Month")')
-    $.each(table_headers, function (index, value) {
-        value.innerHTML = value.innerHTML + '<th>Price Per Month</th>';
-    });
-
     var cells = table_headers.parent().parent().find('tr[data-plc-offer-id]');
+    var modified_cells = false;
     var pattern = new RegExp("^\\$(\\d+\\.\\d+)$");
+
     $.each(cells, function (index, value) {
         var message = $(value).find('td:last-child')[0].innerText;
         let matches = pattern.exec(message);
         if (matches != null && matches.length > 1) {
+            modified_cells = true;
+            console.log("inside")
             let cost = parseFloat(matches[1]);
             let monthlyCost = (cost * 24 * 30.5).toFixed(2);
             value.innerHTML = value.innerHTML + '<td>$' + monthlyCost + '</td>';
         }
     });
+
+    if (modified_cells) {
+        $.each(table_headers, function (index, value) {
+            value.innerHTML = value.innerHTML + '<th>Price Per Month</th>';
+        });
+    }
 }
 
 function error() {
